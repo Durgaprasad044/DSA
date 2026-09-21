5ab00bc93ce78a18e99ce9280524a130d283abf6
Durgaprasad044 <seelamdurgaprasad5442@gmail.com>
2026-09-21 21:45:49 +0530

    Time: 3 ms (5.51%) | Memory: 12.7 MB (12.47%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> result(nums.size());
+        partial_sum(nums.begin(), nums.end(), result.begin());
+        return result;
+    }
+};
